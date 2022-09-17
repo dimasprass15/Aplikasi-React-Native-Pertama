5d8489d9b81d8b1f55b3eeeb3ad03c7ce70c899d
dimasprass15 <coretan.boi@gmail.com>
2022-09-17 22:25:29 +0700

    Push Ketiga di App Js

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react"
+import {Text, View, Image} from "react-native"
 
 const App = () => {
   return (
-    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-      <Text style={{fontSize:20, fontWeight:'bold'}}>Aplikasi Pertama Dimas Prassetya</Text>
+    <View>
+    <View style={{width:100, height:100, backgroundColor:'blue'}}/>
+      <Text>Dimas</Text>
+      <Prassetya />
+      <Text>Fahima</Text>
+      <Photo />
     </View>
   )
+}
+
+const Prassetya = () => {
+  return <Text>Prassetya Dimas</Text>
+}
+
+const Photo = () => {
+  return <Image source={{uri:'https://cdn.britannica.com/68/195168-050-BBAE019A/football.jpg'}} 
+    style={{width:50, height:50}}
+  />
 }
 
 export default App
