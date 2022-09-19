420d5b5e58b079709d2a713781a33fefed694415
dimasprass15 <coretan.boi@gmail.com>
2022-09-19 16:49:11 +0700

    Push Selanjutnya

--- App.js
+++ App.js
@@ -1,28 +1,18 @@
-import { View, Text, Image, TouchableOpacity} from 'react-native'
+import { View, TextInput, Text } from 'react-native'
 import React from 'react'
-
 
 const App = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Image
-        source={{
-          uri: 'https://images.unsplash.com/photo-1516321318423-f06f85e504b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-        }}
-        style={{width: 100, height: 100, borderRadius: 50}}
-      />
-      <Text
-        style={{
-          fontWeight: 'bold',
-          marginTop: 10,
-          fontSize: 16,
-          color: '#3B3B3B',
-        }}>
-        Fahima Nur Zaina Islami Login
-      </Text>
-      <TouchableOpacity style={{backgroundColor: '#15CECE', width:250, height:40, marginTop:5, borderRadius:15}}>
-        <Text style={{fontSize:15, justifyContent:'center', alignItems:'center'}}>Silahkan Login</Text>
-      </TouchableOpacity>
+    <View style={{flex: 1, justifyContent: 'center', margin: 20}}>
+      <Text style={{fontWeight: 'bold'}}>Nama Lengkap</Text>
+      <TextInput style={{borderBottomWidth: 1, marginBottom: 60}} />
+      <Text style={{fontWeight: 'bold'}}>Password</Text>
+      <TextInput style={{borderBottomWidth: 1}} />
+      <View 
+        style={{backgroundColor:'orange', width:350, height:50, marginTop:25, borderRadius:10, justifyContent:'center', alignItems:'center'}}
+      >
+        <Text style={{color:'white', fontSize:16, fontWeight:'bold'}}>Silahkan Login</Text>
+      </View>
     </View>
   );
 }
